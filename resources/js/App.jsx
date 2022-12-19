44056837ab06b04cf1eb857e6a32ade0f72164b3
amm834 <aungmyatmoe834@gmail.com>
2022-12-19 22:21:49 +0630

    fix: move to spa

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import PostIndex from "./components/PostIndex";
 import Nav from "./components/Nav";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NewPost from "./components/NewPost";
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Nav/>
+
             <main className="relative mt-24 container mx-auto max-w-6xl">
-                <PostIndex/>
+                <Routes>
+                    <Route path="/" element={<PostIndex/>}/>
+                    <Route path="/post-new" element={<NewPost/>}/>
+                </Routes>
             </main>
-        </>
+        </BrowserRouter>
     );
 };
 
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/', 'layouts.app');
+
+Route::view('/{any?}', view('layouts.app'))->where('any', '.*');
